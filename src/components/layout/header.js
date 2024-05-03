78aee42934401f24dc8c6ad264415b9420f0d60e
astro348 <astrodaniel348@gmail.com>
2024-05-03 13:36:02 +0300

    remove unused values

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Logout from "pages/logout";
+// import Logout from "pages/logout";
 import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
